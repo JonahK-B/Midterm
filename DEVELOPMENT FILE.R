e4f52250aa5e40c5d72a91c0ec99b6d13f6290aa
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-13 12:50:40 -0400

    Package skeleton
    Remembered to run package skeleton, adjusted acccordingly

--- DEVELOPMENT FILE.R
+++ DEVELOPMENT FILE.R
@@ -3,3 +3,14 @@ library(devtools)
 library(roxygen2)
 
 setwd("~/GitHub/Midterm")
+
+current.code <- as.package("midterm")
+load_all(current.code)
+document(current.code)
+
+
+##example
+y <- c(1,2,3,3)
+lambda <- 3
+
+poisson.lik(lambda, y)
