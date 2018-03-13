496df1cbf4685bfd718551650590fdc2ffb61e07
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-13 17:15:02 -0400

    Ex cred
    added validation method to Rasch

--- DEVELOPMENT FILE.R
+++ DEVELOPMENT FILE.R
@@ -10,9 +10,9 @@ document(current.code)
 
 
 ##examples
-jonah = new("Rasch", name = "Jonah", a = c(.3,.2,1,3,4), y = c(1,1,1,0,0))
-probability(jonah, 1)
-likelihood(jonah, 1)
-prior(1)
+jonah = new("Rasch", name = "Jonah", a = c(5,5,5,10,15), y = c(1,1,1,0,0))
+probability(jonah, 10)
+likelihood(jonah, 10)
+prior(10)
 eap(jonah)
 print(jonah)
