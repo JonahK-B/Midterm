5103350d3c87e3487eca833d34d077c4c3c922e2
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-13 12:59:36 -0400

    Rasch
    Figured out my documentation errors for writing Rasch's man file

--- midterm/R/Rasch.R
+++ midterm/R/Rasch.R
@@ -1,15 +1,17 @@
-
+#' Rasch
+#' 
+#' A class representing a student's test answers
 #' 
 #' An object of the class `Rasch' has the following slots:
 #' \itemize{
-#' \item \code{{name} The name of the student represetned by the Rasch function}
-#' \item \code{{a} A vector of length n containing numbers which represetn the difficulty of each question}
-#' \item \code{{y} An answer vector of length n, where y[i] = 1 if they got the question correct and 0 if they got the question incorrect}
+#' \item \code{name} The name of the student represetned by the Rasch function
+#' \item \code{a} A vector of length n containing numbers which represetn the difficulty of each question
+#' \item \code{y} An answer vector of length n, where yi = 1 if they got the question correct and 0 if they got the question incorrect
 #' 
 #' }
 #'
 #' @author Jonah Klein-Barton: \email{jonahkleinbarton@@gmail.com}
-#' @aliase Rasch-class initialize
+#' @aliases Rasch-class initialize
 #' @rdname Rasch
 #' @export
 setClass(Class="Rasch", 
